aecd96e36c6323d17ff7b2bbb15e28698de19415
邓小宇 <625071541@qq.com>
2021-05-16 14:58:29 +0800

    无聊

--- worker/middleware/setToken.js
+++ worker/middleware/setToken.js
@@ -11,7 +11,7 @@ module.exports = options => {
 		ctx.cookies.set('_csrf', token, {
 			maxAge: 1000 * 60 * 60 * 24, // cookie有效时长
 			path: '/', // 该cookie生效的路径
-			httpOnly: false, // 前端js是否可以访问cookie
+			httpOnly: true, // 前端js是否可以访问cookie
 			overwrite: true, // 如果设置两个相同的key名，是否后者覆盖前者
 			signed: false, //使用签名 默认true
 			encrypt: false, //是否对cookie加密
